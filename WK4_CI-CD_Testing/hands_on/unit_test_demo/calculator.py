1a21640c1c09405a5037a553be78add705366a28
Davis <davisjiangren01@gmail.com>
2021-01-24 15:24:58 +1100

    Add simple hello

--- WK4_CI-CD_Testing/hands_on/unit_test_demo/calculator.py
+++ WK4_CI-CD_Testing/hands_on/unit_test_demo/calculator.py
@@ -3,6 +3,7 @@ class Calculator:
     def __init__(self, a: int, b: int):
         self.a = a
         self.b = b
+        print("this is init")
 
     def add(self):
         return self.a + self.b
@@ -15,3 +16,12 @@ class Calculator:
 
     def divide(self):
         return self.a / self.b
+
+
+
+c = Calculator(12, 2)
+
+print(c.add())
+print(c.minus())
+print(c.multiply())
+print(c.divide())
